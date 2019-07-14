ec5be6ae12a481d4423329dde5615a9421c0f4e2
Ali Baraka <baraka.ali210@gmail.com>
2019-07-14 13:12:38 -0500

    View  the form for creating a post

--- src/main/java/com/Codeup/blogspring/PostController.java
+++ src/main/java/com/Codeup/blogspring/PostController.java
@@ -1,6 +1,7 @@
 package com.Codeup.blogspring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +19,11 @@ public class PostController {
     @ResponseBody
     public String show(@PathVariable int id){
         return "View an individual post";
+    }
+    @RequestMapping("/posts/create")
+    @ResponseBody
+    public String create(){
+        return "View the form for creating a post";
     }
 
 }
