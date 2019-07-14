83869a8a07f4920fc5f9fd6c0f989c0f34973eb0
Ali Baraka <baraka.ali210@gmail.com>
2019-07-14 13:00:45 -0500

    Controllers exercise: division opperation URI response

--- src/main/java/com/Codeup/blogspring/MathController.java
+++ src/main/java/com/Codeup/blogspring/MathController.java
@@ -24,6 +24,25 @@ public class MathController {
         return num1 + " + " + num2 + " = " + (num1 + num2);
     }
 
+    @RequestMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public String subtract (@PathVariable int num1, @PathVariable int num2){
+        return num2 + " - " + num1 + " = " + (num2 - num1);
+    }
+
+    @RequestMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiply (@PathVariable int num1, @PathVariable int num2){
+        return num2 + " x " + num1 + " = " + (num2 * num1);
+    }
+
+    @RequestMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public String divide (@PathVariable int num1, @PathVariable int num2){
+        return num1 + " / " + num2 + " = " + (num1 / num2);
+    }
+
+
 
 
 
