ff646ce5250d9bc6674aca0a193d362096749711
Ali Baraka <baraka.ali210@gmail.com>
2019-07-14 12:23:35 -0500

    Path Variables

--- src/main/java/com/Codeup/blogspring/HelloController.java
+++ src/main/java/com/Codeup/blogspring/HelloController.java
@@ -2,15 +2,26 @@ package com.Codeup.blogspring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Controller /**Defines the class as a controller*/
 public class HelloController {
 
+    /**
+     *  defines a method that should be invoked when
+     *  a GET request is recieved for the specified URI
+     *  */
     @GetMapping("/hello")
-//    @ResponseBody
+    @ResponseBody /**the  return from this method should be the body for the response*/
     public String hello(){
         return "Hello form Spring!";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name){
+        return "Hello form " + name + "!";
     }
 
 }
